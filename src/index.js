5aff7317c641bd36d80257cad70059bcdd022db5
Vivek Sabbani <sabbanivivek@gmail.com>
2022-02-24 12:27:21 +0530

    Added tasks get routes

--- src/index.js
+++ src/index.js
@@ -6,30 +6,54 @@ const Task = require('./models/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/users',(req,res)=>{
     User.find({}).then(users=>{
         if(!users){
-            res.status(404).send();
+            res.status(404).send('Users not found!!!');
         }
         res.send(users);
     }).catch(e=>{
         res.status(500).send(e.message);
     })
-})
+});
 
 app.get('/users/:id',(req,res)=>{
     const _id = req.params.id;
     User.findById(_id).then(user=>{
         if(!user){
             res.status(404).send('User not found!!!');
+        }else{
+            res.send(user);
         }
-        res.send(user);
     }).catch(e=>{
         res.status(500).send(e.message);
     })
-})
+});
+
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then(tasks=>{
+        if(!tasks)
+            res.status(404).send('No tasks are present. Add your tasks.');
+        else
+            res.send(tasks);
+    }).catch(e=>{
+        res.status(500).send(e.message);
+    })
+});
+
+app.get('/tasks/:id',(req,res)=>{
+    const _id = req.params.id;
+    Task.findById(_id).then(task=>{
+        if(!task)
+            res.status(404).send('Requested task is not present!!!');
+        else
+            res.send(task);
+    }).catch(e=>{
+        res.status(500).send(e.message);
+    })
+});
 
 app.post('/users',(req,res)=>{
     const newUser = new User(req.body);
@@ -38,7 +62,7 @@ app.post('/users',(req,res)=>{
     }).catch((e)=>{
         res.status(400).send(e.message);
     });
-})
+});
 
 app.post('/tasks',(req,res)=>{
     const newTask = new Task(req.body);
@@ -47,8 +71,8 @@ app.post('/tasks',(req,res)=>{
     }).catch(e=>{
         res.status(400).send(e.message);
     })
-})
+});
 
 app.listen(port,()=>{
     console.log("Node server started successfully at port: "+port);
-})
+});
