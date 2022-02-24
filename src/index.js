b680d70c304e9b8de8761a44f70da20d93c8a699
Vivek Sabbani <sabbanivivek@gmail.com>
2022-02-24 12:16:56 +0530

    Added users get endpoints

--- src/index.js
+++ src/index.js
@@ -8,6 +8,29 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
+app.get('/users',(req,res)=>{
+    User.find({}).then(users=>{
+        if(!users){
+            res.status(404).send();
+        }
+        res.send(users);
+    }).catch(e=>{
+        res.status(500).send(e.message);
+    })
+})
+
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id;
+    User.findById(_id).then(user=>{
+        if(!user){
+            res.status(404).send('User not found!!!');
+        }
+        res.send(user);
+    }).catch(e=>{
+        res.status(500).send(e.message);
+    })
+})
+
 app.post('/users',(req,res)=>{
     const newUser = new User(req.body);
     newUser.save().then((result)=>{
